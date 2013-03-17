bff477eb095c304222d552c464ce2b3caf41d9ed
Alex Madsen <akmadsen522@gmail.com>
2013-03-17 14:11:55 -0600

    removed useless entity classes/tests, and created environment test units

--- Sisyphus1/test/cpsc433/EnvironmentTest.java
+++ Sisyphus1/test/cpsc433/EnvironmentTest.java
@@ -1,3 +1,6 @@
+/**
+ * 
+ */
 package cpsc433;
 
 import static org.junit.Assert.*;
@@ -6,12 +9,22 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class RoomTest {
+/**
+ * @author akmadsen
+ *
+ */
+public class EnvironmentTest {
 
+	/**
+	 * @throws java.lang.Exception
+	 */
 	@Before
 	public void setUp() throws Exception {
 	}
 
+	/**
+	 * @throws java.lang.Exception
+	 */
 	@After
 	public void tearDown() throws Exception {
 	}
