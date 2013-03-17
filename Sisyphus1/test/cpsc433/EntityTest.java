8a834b23229550b19e238b96cd3538bbfe73784a
Alex Madsen <akmadsen522@gmail.com>
2013-03-17 17:20:59 -0600

    created new entity test suite

---
+++ Sisyphus1/test/cpsc433/EntityTest.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package cpsc433;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author akmadsen
+ *
+ */
+public class EntityTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
