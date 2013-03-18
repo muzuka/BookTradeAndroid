5e6cbd4dd47b3a24f2e7c3788bab8f3e8ab81815
Alex Madsen <akmadsen522@gmail.com>
2013-03-18 03:02:36 -0600

    Asserted that the 'name' constructor works for the PredicateReader

--- Sisyphus1/test/cpsc433/PredicateReaderTest.java
+++ Sisyphus1/test/cpsc433/PredicateReaderTest.java
@@ -8,17 +8,36 @@ import org.junit.Test;
 
 public class PredicateReaderTest {
 
+	PredicateReader myReader; 
+	
 	@Before
 	public void setUp() throws Exception {
+		myReader = new PredicateReader("testReader"); 
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		myReader = null; 
 	}
 
+	/**
+	 * Just makes sure that the name of the PR is properly set
+	 * after being constructed on name alone... But why does 
+	 * a PR need a name? That seems weird to me. 
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testStringConstructor() {
+		assertEquals("testReader", myReader.getName());
 	}
-
+	
+	//testCopyConstructor(PredicateReader)
+	//testFromFile(String)
+	//testFromStream(BufferedReader)
+	//testAssert_(String)
+	//testEval(String)
+	//testAssert_(Predicate)
+	//testEval(Predicate)
+	//testA_read(String)
+	//testA_help(String)
+	//testError(String)
 }
