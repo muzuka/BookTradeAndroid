847d54c07f6a4159839e87487a01137ca9efa6fe
Alex Madsen <akmadsen522@gmail.com>
2013-03-28 14:48:15 -0600

    updated the solution commentary in my effort to understand it

--- Sisyphus1/src/cpsc433/Solution.java
+++ Sisyphus1/src/cpsc433/Solution.java
@@ -1,6 +1,9 @@
 package cpsc433;
 
+import java.util.ArrayList;
+
 public class Solution {
+	private ArrayList<Predicate> assignments;
 	
 	public enum Verbosity {
 		SUMMARY;
@@ -8,21 +11,31 @@ public class Solution {
 	public static Verbosity verbosity;
 
 	public Solution(String outfilename) {
-		// TODO Auto-generated constructor stub
+		this.assignments = new ArrayList<Predicate>(); 
 	}
 
 	public int getGoodness() {
 		// TODO Auto-generated method stub
+		
+		// place in the soft constraints and their corresponding
+		// penalties - requires knowledge of the environment... Hurm... 
+		// seems like this was what the Singleton pattern was designed for... 
+		// will this work well? - AM 
 		return 0;
 	}
 
 	public boolean isComplete() {
 		// TODO Auto-generated method stub
+		
+		// if all of the persons are assigned, then the solution
+		// is complete
 		return false;
 	}
 
 	public boolean isSolved() {
 		// TODO Auto-generated method stub
+		
+		// how is this different from isComplete() for Otrees? 
 		return false;
 	}
 
