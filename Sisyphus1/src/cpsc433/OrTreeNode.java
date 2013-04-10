d98966359a9e800e0099836f7371e73d972ce67e
akmadsen <akmadsen522@gmail.com>
2013-04-10 12:43:04 -0600

    Updated the commentary for OrTreeNode

--- Sisyphus1/src/cpsc433/OrTreeNode.java
+++ Sisyphus1/src/cpsc433/OrTreeNode.java
@@ -22,27 +22,11 @@ public class OrTreeNode {
 	private ArrayList<Pair<Entity, Entity>> assigned;
 	private ArrayList<OrTreeNode> children;
 	
-	// Give this a person
-	private boolean isAssigned(Entity e) {
-		for (Pair<Entity, Entity> p : assigned) {
-			if (p.getKey().compareTo(e) == 0) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	// Give this a room
-	private int numAssigned(Entity e) {
-		int i = 0;
-		for (Pair<Entity, Entity> p : assigned) {
-			if (p.getValue().compareTo(e) == 0) {
-				i++;
-			}
-		}
-		return i;
-	}
-	
+	/**
+	 * Creates a new OrTreeNode with the current assignments
+	 * @param assigned The current assignments. Pass in 
+	 * <b><code>null</code></b>to create a root node. 
+	 */
 	public OrTreeNode(ArrayList<Pair<Entity, Entity>> assigned) {
 		if (assigned == null) {
 			this.assigned = new ArrayList<Pair<Entity, Entity>>();
@@ -54,6 +38,44 @@ public class OrTreeNode {
 		env = Environment.get();
 	}
 	
+	
+	/** 
+	 * Checks whether the given person is assigned. 
+	 * @param e Checks whether the person is currently assigned. 
+	 * @return True if the person is assigned, false otherwise. 
+	 */
+	private boolean isAssigned(Entity e) {
+		for (Pair<Entity, Entity> p : assigned) {
+			if (p.getKey().compareTo(e) == 0) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Returns the number of persons assigned to the room. 
+	 * @param e The room to be checked. 
+	 * @return The number of persons assigned to a room. 
+	 */
+	// Give this a room
+	private int numAssigned(Entity e) {
+		int i = 0;
+		for (Pair<Entity, Entity> p : assigned) {
+			if (p.getValue().compareTo(e) == 0) {
+				i++;
+			}
+		}
+		return i;
+	}
+	
+	/**
+	 * Search for the optimal room assignment given the 
+	 * state of the current environment. If Ctrl+C is pressed
+	 * or the process is otherwise interrupted, the current 
+	 * solution is returned. 
+	 * @return the OrTreeNode with the optimal found solution. 
+	 */
 	// This isn't done. Don't complain just yet. - T
 	public OrTreeNode search() {
 		if (assign() == 0) {
@@ -67,6 +89,12 @@ public class OrTreeNode {
 		return null;
 	}
 	
+	/**
+	 * Assign a person to a room 
+	 * TODO: should this be in the Solution/Involve the Solution? 
+	 * @return 1 if the person is a group head 
+	 * 			
+	 */
 	public int assign() {
 		ArrayList<Entity> groupHeads = env.getGroupHeads();
 		for (Entity p : groupHeads) {
@@ -77,7 +105,11 @@ public class OrTreeNode {
 		}
 		return 0;
 	}
-	
+
+	/**
+	 * Don't quite know what this is doing... We'll figure it out. 
+	 * @param p 
+	 */
 	// One per room, Should be a large room
 	// These are assigned first
 	private void assignGroupHead(Entity p) {
