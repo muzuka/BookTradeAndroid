11d220264ff6421ce5e0dad1ba67562331b339af
muzuka <seanbrown7935@gmail.com>
2014-12-06 15:10:38 -0700

    Added return type to functions. Also testing Android Studio Git.

--- src/com/example/booktradeapp/MainActivity.java
+++ src/com/example/booktradeapp/MainActivity.java
@@ -33,18 +33,16 @@ public class MainActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
-    public static searchBar()
+    public static void searchBar()
     {
     		//needs to put a search bar on the screen
     		//needs to take user input
     		// call thing that returns books
     }
-    public static searchResults()
+    public static void searchResults()
     {
     	//takes input from searchBar
-    	<?php
-    		//quieries database	
-    	?>
+
     	//returns results, prefribly in a grid format.
     }
 }
