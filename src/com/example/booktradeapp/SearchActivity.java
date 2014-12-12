7fe552098d12a9c467f64ee7c3e680a505b16a9f
muzuka <seanbrown7935@gmail.com>
2014-12-11 23:37:29 -0700

    doInBackground is almost done. Only valid link and the linked page is required.

--- src/com/example/booktradeapp/SearchActivity.java
+++ src/com/example/booktradeapp/SearchActivity.java
@@ -1,6 +1,17 @@
 package com.example.booktradeapp;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 import android.os.AsyncTask;
+import org.apache.http.*;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 /**
  * Created by seanbrown on 2014-12-11.
@@ -11,11 +22,30 @@ public class SearchActivity extends AsyncTask<String, Void, String> {
     protected String doInBackground(String... arg0) {
         try {
             String query = arg0[0];
+            String link = new String();
+            String data = URLEncoder.encode("query", "UTF-8") + "=" + URLEncoder.encode(query, "UTF-8");
 
-            return "";
+            URL address = new URL(link);
+            HttpClient client = new DefaultHttpClient();
+
+            URLConnection connection = address.openConnection();
+            connection.setDoOutput(true);
+            OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+            writer.write(data);
+            writer.flush();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String output = null;
+            String line = null;
+
+            while((line = reader.readLine()) != null) {
+                output = output + line;
+            }
+
+            return output;
         }
         catch(Exception e) {
-            return new String(e.getMessage());
+            return e.getMessage();
         }
     }
 
