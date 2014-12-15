5d86c49ff15e35f3a45e4feabb498ede5523c2bb
muzuka <seanbrown7935@gmail.com>
2014-12-15 11:59:37 -0700

    removed Asynctask superclass and replaced with Activity superclass also added onCreate function

--- src/com/example/booktradeapp/SearchActivity.java
+++ src/com/example/booktradeapp/SearchActivity.java
@@ -8,22 +8,39 @@ import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
+
+import android.app.SearchManager;
 import android.os.AsyncTask;
 import org.apache.http.*;
 import org.apache.http.client.HttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
+import android.content.Intent;
+import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by seanbrown on 2014-12-11.
  */
-public class SearchActivity extends AsyncTask<String, Void, String> {
+public class SearchActivity extends Activity {
+
+    private String results;
 
     @Override
-    protected String doInBackground(String... arg0) {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        if(Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            results = search(query);
+        }
+    }
+
+    protected String search(String q) {
         try {
-            String query = arg0[0];
             String link = new String();
-            String data = URLEncoder.encode("query", "UTF-8") + "=" + URLEncoder.encode(query, "UTF-8");
+            String data = URLEncoder.encode("query", "UTF-8") + "=" + URLEncoder.encode(q, "UTF-8");
 
             URL address = new URL(link);
             HttpClient client = new DefaultHttpClient();
