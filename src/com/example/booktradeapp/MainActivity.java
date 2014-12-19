1c17d36a1b974ab3571583007f4975fb82dbe00a
muzuka <seanbrown7935@gmail.com>
2014-12-19 12:14:08 -0700

    moved search to SearchActivity and defined onQueryTextListener

--- src/com/example/booktradeapp/MainActivity.java
+++ src/com/example/booktradeapp/MainActivity.java
@@ -1,17 +1,78 @@
 package com.example.booktradeapp;
 
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.content.Context;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 
 public class MainActivity extends Activity {
+
+    private ListView listView;
+    private SearchView searchView;
+    private ArrayList<String> results;
+    private Context thisContext = this;
+    private AsyncTask<String, Void, ArrayList<String>> activity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView)findViewById(R.id.listView1);
+        searchView = (SearchView)findViewById(R.id.searchView1);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                activity = new SearchActivity(thisContext).execute(query);
+
+                try {
+                    results = activity.get();
+                }
+                catch(InterruptedException e) {
+                    e.printStackTrace();
+                    return false;
+                }
+                catch(ExecutionException e) {
+                    e.printStackTrace();
+                    return false;
+                }
+
+                if(results == null) {
+                    results = new ArrayList<String>();
+                }
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(thisContext, R.layout.activity_main, results);
+                listView.setAdapter(adapter);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                onQueryTextSubmit(newText);
+                return false;
+            }
+        });
     }
 
 
@@ -33,5 +94,7 @@ public class MainActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
 
 }
