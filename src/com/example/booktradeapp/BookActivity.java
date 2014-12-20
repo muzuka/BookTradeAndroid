890afe0cb37b33adc8f6c8f4a73b0ee3df064635
muzuka <seanbrown7935@gmail.com>
2014-12-20 16:15:35 -0700

    created new book activity file for viewing books

---
+++ src/com/example/booktradeapp/BookActivity.java
@@ -0,0 +1,16 @@
+
+/*
+ * Book Activity class
+ * Views book page
+ *
+ * Author: Sean Brown, Laura Berry, Andrew Lata
+ */
+
+package com.example.booktradeapp;
+
+import android.app.Activity;
+
+public class BookActivity extends Activity {
+
+
+}
