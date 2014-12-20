e9a3b00cc89adc809ad3da8ca1bcc2c22ddeafe1
muzuka <seanbrown7935@gmail.com>
2014-12-20 12:18:25 -0700

    changed task return type to two arraylists

--- src/com/example/booktradeapp/SearchActivity.java
+++ src/com/example/booktradeapp/SearchActivity.java
@@ -3,6 +3,7 @@ package com.example.booktradeapp;
 
 import android.os.AsyncTask;
 import android.content.Context;
+import android.util.Pair;
 
 import org.apache.http.client.HttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -15,7 +16,7 @@ import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 
-public class SearchActivity extends AsyncTask<String, Void, ArrayList<String>> {
+public class SearchActivity extends AsyncTask<String, Void, Pair<ArrayList<String>, ArrayList<String> > > {
 
     private String link = new String("http://10.0.2.2/BookTrade/appSearchPage.php");
     private String data;
@@ -25,14 +26,14 @@ public class SearchActivity extends AsyncTask<String, Void, ArrayList<String>> {
     private HttpClient client;
     private BufferedReader reader;
     private OutputStreamWriter writer;
-    private ArrayList<String> output;
+    private Pair<ArrayList<String>, ArrayList<String> > output;
 
     protected SearchActivity(Context context) {
         this.context = context;
     }
 
     @Override
-    protected ArrayList<String> doInBackground(String... args) {
+    protected Pair<ArrayList<String>, ArrayList<String> > doInBackground(String... args) {
         try {
             data = URLEncoder.encode("query", "UTF-8") + "=" + URLEncoder.encode(args[0], "UTF-8");
 
@@ -47,19 +48,24 @@ public class SearchActivity extends AsyncTask<String, Void, ArrayList<String>> {
             writer.flush();
 
             reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            output = new ArrayList<String>();
+
+            output = new Pair<ArrayList<String>, ArrayList<String> >(new ArrayList<String>(), new ArrayList<String>());
             String[] rows;
             String[] rowElements;
             String line = null;
 
             while((line = reader.readLine()) != null) {
+                if(line.length() == 0) {
+                    continue;
+                }
                 if(line.charAt(0) == ':' && line.charAt(1) == ':') {
                     rows = line.split("::");
                     for(int i = 0; i < rows.length; i++) {
                         if(rows[i].length() != 0) {
-                            rowElements = rows[i].split("/");
+                            rowElements = rows[i].split(":");
 
-                            output.add(rowElements[0] + " " + rowElements[1] + " " + rowElements[2]);
+                            output.first.add(rowElements[0] + " " + rowElements[1] + " " + rowElements[2]);
+                            output.second.add(rowElements[3]);
                         }
                     }
                     break;
